0d0cc8326f19cc55aed33a104264763cc098f897
robsonklima <robsonklima#gmail.com>
2017-07-10 09:41:33 -0300

    Code ref

--- db.js
+++ db.js
@@ -32,7 +32,14 @@ switch(env) {
 }
 
 exports.connect = function() {
-  pool = mysql.createPool(dbConfig);
+  pool = mysql.createPool(dbconfig = {
+          connectionLimit : 100,
+          host     : 'us-cdbr-iron-east-03.cleardb.net',
+          user     : 'b544072879e803',
+          password : '8de4e484',
+          database : 'heroku_45af283e6046208',
+          debug    :  false
+        });
 }
 
 exports.get = function() {
